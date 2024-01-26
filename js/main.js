063082a56683d67f9a229257bb9f20171115b5f0
taydoedu <154655304+taydoedu@users.noreply.github.com>
2024-01-26 15:35:10 +0700

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,20 +1,3 @@
- // THÔNG TIN
- const twElement = document.getElementById("tw");
-        twElement.innerHTML = "CHINH PHỤC THPT";
-        const dvqlElement = document.getElementById("dvql");
-        dvqlElement.innerHTML = "Trường THPT Tây Đô";
-        const dcaxElement = document.getElementById("dcax");
-        dcaxElement.innerHTML = "<a style='color: #fff; text-decoration: none;' href='https://maps.app.goo.gl/bfW6VKETVL6A2tDv7'>" + "&nbsp"+"Ấp 1 - Thị trấn Vĩnh Viễn"  ;
-        const dchtElement = document.getElementById("dcht");
-        dchtElement.innerHTML = "<a style='color: #fff; text-decoration: none;' href='https://maps.app.goo.gl/bfW6VKETVL6A2tDv7'>" + "&nbsp"+"Huyện Long Mỹ - Hậu Giang"  ;
-        const sdtElement = document.getElementById("sdt");
-        sdtElement.innerHTML = "<a style='color: #fff; text-decoration: none;' href='tel://0334288374'>" + "&nbsp"+"0334288374"  ;
-        const mailtruongElement = document.getElementById("mailtruong");
-        mailtruongElement.innerHTML = "<a style='color: #fff; text-decoration: none;' href='mailto:c3taydo@haugiang.edu.vn?subject=Liên hệ&body='>" + "&nbsp"+"c3taydo@haugiang.edu.vn"  ;
-        const lhElement = document.getElementById("lh");
-        lhElement.innerHTML = "<a style='color: #fff; text-decoration: none;' href='https://m.me/thanhloc.huynh.5249'><img src='https://thanhvanlongcode.files.wordpress.com/2023/06/1687176639342.png' style='width: 50px'></a><a style='color: #fff; text-decoration: none;' href='http://zaloapp.com/qr/p/wwr1vqrlvd42'><img src='https://thanhvanlongcode.files.wordpress.com/2023/06/1687176669932.png' style='width: 50px'></a><a style='color: #fff; text-decoration: none;' href='https://instagram.com/lochuynh.2008?igshid=Mzc0YWU1OWY='><img src='https://thanhvanlongcode.files.wordpress.com/2023/06/1687176702024.png' style='width: 50px'></a>";
-        const tkhdElement = document.getElementById("tkhd");
-        tkhdElement.innerHTML =" <a style='color: #fff; text-decoration: none; font-size: 10px;' href='https://github.com'>" + "&nbsp"+"Github</a>" +"&nbsp" + "và" + "<a style='color: #fff; text-decoration: none; font-size: 10px;' href='https://glitch.com'>" + "&nbsp"+"Glitch"  ;
 // SLIDE
  function myFunction() {
             var x = document.getElementById("noi_dung_cua_toi");
